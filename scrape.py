1ae119a3ced097510c7e7be0bc016c5b5b990c41
simprisms <59995894+simprisms@users.noreply.github.com>
2021-04-01 15:54:35 +0200

    Update scrape.py

--- scrape.py
+++ scrape.py
@@ -7,6 +7,7 @@ timestr = time.strftime("%Y%m%d")
 
 url_counties = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_county_condensed_data"
 url_country = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_data"
+url_historical = "https://covid.cdc.gov/covid-data-tracker/COVIDData/getAjaxData?id=vaccination_trends_data"
 
 response = requests.get(url_counties)
 data = response.json()
@@ -20,3 +21,9 @@ country_data = country_response.json()
 
 with open(f"data/{timestr}_country_data.json", "w") as country_outfile:
     json.dump(country_data, country_outfile)
+
+response_historical = requests.get(url_historical)
+historical_data = response_historical.json()
+
+with open(f"data/{timestr}_us_historical.json", "w") as historical_outfile:
+    json.dump(historical_data, historical_outfile)
